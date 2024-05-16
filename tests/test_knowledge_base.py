78996f599afe89c4b38b44f157b9cb91ec771d6a
MyLadyDarkness <Cleopathra@yandex.ru>
2024-05-16 23:50:36 +0300

    fix fixture

--- tests/test_knowledge_base.py
+++ tests/test_knowledge_base.py
@@ -1,5 +1,9 @@
-import allure
+import time
 
+import allure
+from selene import browser, have
+
+from utils import attach
 from data.knowledge_search import BaseSearch
 from model.pages.open_page import OpenPage
 
@@ -13,12 +17,29 @@ def test_knowledge_base():
         page.open_start_page()
 
     with allure.step("Переход в базу знаний"):
-        page.open_knowledge_base()
-
-    base_search = BaseSearch()
+        browser.element('a[href="https://help.telega.in/hc/ru"]').click()
 
     with allure.step("Поиск в базе знаний по заданному запросу"):
-        base_search.search_request(search_value)
+        browser.element('#query[name=query]').type(search_value)
+        browser.element('#query[name=query]').press_enter()
 
-    with allure.step("Появление капчи, провеяющей, ввел запрос человек или робот"):
-        base_search.check_search_security()
+    with allure.step("Поиск в базе знаний по заданному запросу"):
+        browser.all('.results-list-item-link').first.should(have.text(search_value))
+
+
+    # with allure.step("Открывается стартовая страница"):
+    #     page.open_start_page()
+    #
+    # with allure.step("Переход в базу знаний"):
+    #     page.open_knowledge_base()
+    #
+    # base_search = BaseSearch()
+    #
+    # with allure.step("Поиск в базе знаний по заданному запросу"):
+    #     base_search.search_request(search_value)
+    #     attach.add_screenshot(browser)
+    #
+    # #time.sleep(50)
+    # with allure.step("Появление капчи, провеяющей, ввел запрос человек или робот"):
+    #     base_search.check_search_security()
+    #     attach.add_screenshot(browser)
