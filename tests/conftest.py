0364dfd052f489cf5cfcfadc74ac39f6b1be604b
MyLadyDarkness <Cleopathra@yandex.ru>
2024-05-16 16:07:45 +0300

    add attachments and selenoid-jenkins integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,30 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
+options = Options()
+selenoid_capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "100.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": True
+    }
+}
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -9,3 +34,11 @@ def browser_settings():
     browser.config.window_width = 1500
     yield
     browser.quit()
+
+
+options.capabilities.update(selenoid_capabilities)
+driver = webdriver.Remote(
+    command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+    options=options)
+
+browser.config.driver = driver
--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -1,7 +1,8 @@
 import allure
-
+from selene import browser
 from data.analytics import Analytics
 from model.pages.open_page import OpenPage
+from utils import attach
 
 
 def test_analytics_filter():
@@ -17,6 +18,9 @@ def test_analytics_filter():
 
     with allure.step("Проверяется, что фильтр выдает хотя бы один релевантный результат"):
         analytics.check_filter("leod")
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
 
 
 def test_add_channel():
@@ -32,3 +36,4 @@ def test_add_channel():
 
     with allure.step("Проверяется, что нельзя добавить канал без ссылки"):
         channel.check_add_channel()
+        attach.add_screenshot(browser)
